b8bb30d4e164f526bf9943d9ce5212cf38946c60
iviondayjr <patrick.montag@gmail.com>
2023-05-08 20:30:30 -0500

    Add skip Now Playing text

--- commands/skip.js
+++ commands/skip.js
@@ -6,15 +6,35 @@ module.exports = {
     .setDescription('Plays next song in queue.'),
   async execute(interaction) {
 
-    interaction.client.spotifyApi.skipToNext()
+    const spotifyApi = interaction.client.spotifyApi;
+    await spotifyApi.ensureAccessToken();
+
+    await spotifyApi.skipToNext()
       .then(function () {
         console.log('Playback skipped');
       }, function (err) {
         console.log('Something went wrong!', err);
       });
 
-    //TODO: add message feedback in discord channel
+    // Delay one second to wait for Spotify skip.
+    await new Promise(resolve => setTimeout(resolve, 1000));
 
-    await interaction.reply('Ok!');
+    const replyText = await spotifyApi.getMyCurrentPlaybackState()
+      .then(function (data) {
+        // Output items
+        if (data.body && data.body.is_playing) {
+          const item = data.body.item;
+          return `Now playing: ${item.artists[0]?.name} - ${item?.name}`;
+        } else {
+          return 'Not playing anything.';
+        }
+      }, function (err) {
+        console.log('Something went wrong!', err);
+        return 'Error getting playback state.';
+      });
+
+    console.log(replyText);
+
+    await interaction.reply(replyText);
   },
 };
